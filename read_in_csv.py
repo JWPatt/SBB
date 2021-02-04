672f8f0a2ad8ff46b3590c553c9b166e1f398260
JWPatt <jamespatterson1989@gmail.com>
2021-02-04 23:04:56 +0100

    cleaning up, reorganizing

--- read_in_csv.py
+++ read_in_csv.py
@@ -1,9 +1,5 @@
 import pandas as pd
 
-def csv_to_set(file_name):
-    names = pd.read_csv(file_name, sep=',',header=0, encoding="ISO-8859-1")['Name'].to_set()
-    return names
-
 def betriebspunkt_to_dict(all_city_file_name):
     names = pd.read_csv(all_city_file_name, sep=',',header=0, encoding="ISO-8859-1")['Name'].to_dict()
     return names
@@ -14,3 +10,8 @@ def read_intermediate_data(intermediate_data_name):
     for i in range(0,len(df[0])):
         data_segment[df[0][i]] = [df[1][i], df[2][i], df[3][i]]
     return data_segment
+
+def read_list_of_cities(cities_file_name):
+    data_portion = set(pd.read_csv(cities_file_name, header=None, encoding="ISO-8859-1")[0])
+    data_portion = {key: None for key in data_portion}
+    return data_portion
