2adb6bbdb85306b379ffab79c83fd6a6f7c06ebd
JWPatt <jamespatterson1989@gmail.com>
2021-09-26 23:03:59 +0200

    - cleaning and applying some better practices (string formatting, enumerate rather than for i in range(len(...)), etc.)

--- core_func/sbb_api_asyncio.py
+++ core_func/sbb_api_asyncio.py
@@ -1,72 +1,48 @@
-import requests
 import time
-from requests.adapters import HTTPAdapter
-from requests.packages.urllib3.util.retry import Retry
-from pprint import pprint
 import datetime
 import core_func
-import multiprocessing as mp
 import aiohttp
 import asyncio
 
 
-# I just discovered a different API when allows for multiple destination searching - up to ~200 at a time!
-# Total game changer, totally reshapes how the API queries will be structured. Eliminates the need for the
-# parallelization, nearly.
-
-# Since I need to rewrite this, I will add features that I determined would be useful while working on this app.
-# old features: destination, lat, long, duration
-# new features:
-#   arrival time [int] - let the map be made by arrival time or travel duration
-#   num transfers [int] - as above, mutatis mutandis
-#   is terminus [bool] - this API lists terminus stations for each connection, which helps tremendously
-#   typo [string] - link the misspelled destination to the real destination
-#   intermediate stations [list of strings] - list the stations between origin and destination, allows for some cool
-#           visualizations later
-#
-# Take this opportunity to restructure how the results are communicated, too:
-# old way: {destination: [lon, lat, trip_duration]}
-# Since many lookups are done, we want to keep it in a dict, but want the data inside to be more intuitive
-# new way: {destination: {lon: lon, lat: lat, trip_duration: trip_duration, arrival_time: arrival_time,
-#                           num_connections: num_connections
-
-# Because jobs are spawned prior to the dict being filled in, a worker may be assigned to query
-# a destination for which we already have a duration. Therefore, we pass in the entire data dict, allowing
-# the worker to check before wasting a precious API query.
-
 # Queries and processes the timetable.search.ch API
 async def async_query_and_process(origin_details, destination_list, session):
-    url = create_url([origin_details, '2021-06-25','7:00'], destination_list)
+    travel_start_date = '2021-06-25'
+    travel_start_time = '7:00'
+
+    url = create_url([origin_details, travel_start_date, travel_start_time], destination_list)
 
     t_init = time.time()
     async with session.get(url) as response:
+        # TODO: replace this print and the time stamping with proper logging
         print(response)
         output_data_portion = await asyncio_process_response(response)
         td_get = time.time() - t_init
+
     return output_data_portion
 
 
 # Takes in the origin details and list of destinations and creates an appropriate url to get
 def create_url (origin_details, destination_list):
     prefix = 'https://timetable.search.ch/api/route.json?one_to_many=1'
-    origin_body = '&from=' + origin_details[0] + '&date=' + origin_details[1] + '&time=' + origin_details[2]
+
+    origin_body = f'&from={origin_details[0]}&date={origin_details[1]}&time={origin_details[2]}'
+
+    # Build iterative with necessary syntax between destinations
     destination_body = ''
-    for i in range(len(destination_list)):
-        destination_body = destination_body + '&to[' + str(i) + ']=' + destination_list[i]
-    url = prefix + origin_body + destination_body
-    print(url)
-    return url
+    for i, dest in enumerate(destination_list):
+        destination_body = f'{destination_body}&to[{i}]={dest}'
+
+    return f'{prefix}{origin_body}{destination_body}'
 
 
 # Processes the response using asyncio
 async def asyncio_process_response(response):
     jdata = await response.json()
-    output = process_response(response, jdata)
-    print( "returning output")
-    return output
+    return process_response(response, jdata)
 
 
-# Processes response and converts the json into a list of destinations with coordinates and travel times
+# Processes response and convert the json into a list of destinations with coordinates and travel times
 def process_response(response, jdata):
     data_portions = []
     if response.status != 200:
@@ -79,76 +55,71 @@ def process_response(response, jdata):
             print("ERROR: API returned with no results.")
             return {}
 
-    data_portions.append({jdata['results'][0]['points'][0]['text'] : {'destination': jdata['results'][0]['points'][0]['text'],
-                                                      'lon': jdata['results'][0]['points'][0]['lon'],
-                                                      'lat': jdata['results'][0]['points'][0]['lat'],
-                                                      'departure': 0,
-                                                      'arrival': 0,
-                                                      'train_time': 0,
-                                                      'num_transfers': 0,
-                                                      'intermediate_stations': 0,
-                                                      'endnode': 0,
-                                                      'hovertext': jdata['results'][0]['points'][0]['text']
-                                                      }})
+    data_portions.append({jdata['results'][0]['points'][0]['text'] :
+                         {'destination': jdata['results'][0]['points'][0]['text'],
+                          'lon': jdata['results'][0]['points'][0]['lon'],
+                          'lat': jdata['results'][0]['points'][0]['lat'],
+                          'departure': 0,
+                          'arrival': 0,
+                          'train_time': 0,
+                          'num_transfers': 0,
+                          'intermediate_stations': 0,
+                          'endnode': 0,
+                          'hovertext': jdata['results'][0]['points'][0]['text'],
+                          }})
 
-    for i in range(len(jdata['results'])):  # iterate on the list of destinations given
+    # Iterate on the list of destinations given
+    for i in range(len(jdata['results'])):
         if 'connections' not in jdata['results'][i]:
-            # if 'message' in jdata['results'][i]:
-            #     data_portion = {jdata['results'][i]: {'error': jdata['results'][i]['message']}}
-            # else:
-            #     data_portion = {jdata['results'][i]: {'error': 'unknown error.'}}
-            # data_portions.append(data_portion)
             continue
-        for con in jdata['results'][i]['connections']:  # iterate on the connection for each destination
-            # if con['departure'].split()[0] != origin_details[1]:
-            #     continue
+
+        # iterate on the connection for each destination
+        for con in jdata['results'][i]['connections']:
             data_portion = {}
             departure_time = datetime_to_timestamp(con['departure'])
             stop_count = 0
-            for leg in range(len(con['legs'])):  # iterate on the legs for each connection
+
+            # iterate on the legs for each connection
+            for leg in range(len(con['legs'])):
                 end_node = 0
                 if 'exit' in con['legs'][leg]:
                     if 'to' in con['legs'][leg]:
-                        if con['legs'][leg]['exit']['name'] == con['legs'][leg]['to']: end_node = 1
-                    data_portion[con['legs'][leg]['exit']['name']] = {'destination': con['legs'][leg]['exit']['name'],
-                                                                      'lon': con['legs'][leg]['exit']['lon'],
-                                                                      'lat': con['legs'][leg]['exit']['lat'],
-                                                                      'departure': departure_time,
-                                                                      'arrival': datetime_to_timestamp(
-                                                                          con['legs'][leg]['exit']['arrival']),
-                                                                      'train_time': datetime_to_timestamp(
-                                                                          con['legs'][leg]['exit'][
-                                                                              'arrival']) - departure_time,
-                                                                      'num_transfers': leg - 1,
-                                                                      'intermediate_stations': stop_count,
-                                                                      'endnode': end_node,
-                                                                      'hovertext': con['legs'][leg]['exit'][
-                                                                                       'name'] + '<br>' + core_func.sec_to_hhmm(
-                                                                          datetime_to_timestamp(
-                                                                              con['legs'][leg]['exit'][
-                                                                                  'arrival']) - departure_time)
-                                                                      }
+                        if con['legs'][leg]['exit']['name'] == con['legs'][leg]['to']:
+                            end_node = 1
+                    data_portion[con['legs'][leg]['exit']['name']] = \
+                        {'destination': con['legs'][leg]['exit']['name'],
+                         'lon': con['legs'][leg]['exit']['lon'],
+                         'lat': con['legs'][leg]['exit']['lat'],
+                         'departure': departure_time,
+                         'arrival': datetime_to_timestamp(con['legs'][leg]['exit']['arrival']),
+                         'train_time': datetime_to_timestamp(con['legs'][leg]['exit']['arrival']) - departure_time,
+                         'num_transfers': leg - 1,
+                         'intermediate_stations': stop_count,
+                         'endnode': end_node,
+                         'hovertext': con['legs'][leg]['exit']['name'] + '<br>' + core_func.sec_to_hhmm(
+                             datetime_to_timestamp(con['legs'][leg]['exit']['arrival']) - departure_time)
+                         }
+
                 if 'stops' not in con['legs'][leg]:
                     continue
+
                 if ('departure' not in con['legs'][leg]) | (con['legs'][leg]['stops'] is None):
                     end_node = 0
-                    if 'departure' not in con['legs'][leg]: end_node = 1
-                    data_portion[con['legs'][leg]['name']] = {'destination': con['legs'][leg]['name'],
-                                                              'lon': con['legs'][leg]['lon'],
-                                                              'lat': con['legs'][leg]['lat'],
-                                                              'departure': departure_time,
-                                                              'arrival': datetime_to_timestamp(
-                                                                  con['legs'][leg]['arrival']),
-                                                              'train_time': datetime_to_timestamp(
-                                                                  con['legs'][leg]['arrival']) - departure_time,
-                                                              'num_transfers': leg - 1,
-                                                              'intermediate_stations': stop_count,
-                                                              'endnode': end_node,
-                                                              'hovertext': con['legs'][leg][
-                                                                               'name'] + '<br>' + core_func.sec_to_hhmm(
-                                                                  datetime_to_timestamp(
-                                                                      con['legs'][leg]['arrival']) - departure_time)
-                                                              }
+                    if 'departure' not in con['legs'][leg]:
+                        end_node = 1
+                    data_portion[con['legs'][leg]['name']] = \
+                        {'destination': con['legs'][leg]['name'],
+                         'lon': con['legs'][leg]['lon'],
+                         'lat': con['legs'][leg]['lat'],
+                         'departure': departure_time,
+                         'arrival': datetime_to_timestamp(con['legs'][leg]['arrival']),
+                         'train_time': datetime_to_timestamp(con['legs'][leg]['arrival']) - departure_time,
+                         'num_transfers': leg - 1,
+                         'intermediate_stations': stop_count,
+                         'endnode': end_node,
+                         'hovertext': con['legs'][leg]['name'] + '<br>' +
+                            core_func.sec_to_hhmm(datetime_to_timestamp(con['legs'][leg]['arrival']) - departure_time)
+                         }
                     continue
 
                 for stop in con['legs'][leg]['stops']:  # iterate on the stops for each leg
@@ -156,23 +127,22 @@ def process_response(response, jdata):
                         continue
                     train_time = datetime_to_timestamp(stop['arrival']) - departure_time
                     if train_time < 86400:
-                        data_portion[stop['name']] = {'destination': stop['name'],
-                                                      'lon': stop['lon'],
-                                                      'lat': stop['lat'],
-                                                      'departure': departure_time,
-                                                      'arrival': datetime_to_timestamp(stop['arrival']),
-                                                      'train_time': train_time,
-                                                      'num_transfers': leg,
-                                                      'intermediate_stations': stop_count,
-                                                      'endnode': 0,
-                                                      'hovertext': stop['name'] + '<br>' + core_func.sec_to_hhmm(
-                                                          train_time)
-                                                      }
+                        data_portion[stop['name']] = \
+                            {'destination': stop['name'],
+                             'lon': stop['lon'],
+                             'lat': stop['lat'],
+                             'departure': departure_time,
+                             'arrival': datetime_to_timestamp(stop['arrival']),
+                             'train_time': train_time,
+                             'num_transfers': leg,
+                             'intermediate_stations': stop_count,
+                             'endnode': 0,
+                             'hovertext': f"{stop['name']}<br>{core_func.sec_to_hhmm(train_time)}",
+                             }
                         stop_count += 1
             data_portions.append(data_portion)
 
-    # data portions contains many multiple entries; now go through and consolidate them
-    # the dictionary with the shorted train_time will be kept; departure time is 2nd priority
+    # Data portions contains many multiple entries; delete duplicates (while minimizing train_time)
     output_data_portion = {}
     for conn in data_portions:  # iterate through each data_portion (ie each connection)
         for city in conn:  # iterate through each destination
@@ -186,7 +156,7 @@ def process_response(response, jdata):
             else:
                 continue
 
-    # if any of the data portions suggest a destination is not an endnode, then it's definitely not
+    # If the city is not an endnode in any of the entries, then flag the destination as not an endnode
     for city in output_data_portion:
         for conn in data_portions:
             if city in conn:
@@ -211,12 +181,11 @@ async def async_api_handler(origin_details, data_set_master, dest_per_query):
             get_reqs.append(asyncio.ensure_future(async_query_and_process(origin_details, dest_list, session)))
         results_list = await asyncio.gather(*get_reqs)
 
-
     results = {}
     for result in results_list:
         results.update(result)
-    print('Time to clear the stack: ' + str(time.time() - t_init) + ' seconds, and ' + str(len(destination_chunks)) + 'API queries')
 
+    print('Time to clear the stack: ' + str(time.time() - t_init) + ' seconds, and ' + str(len(destination_chunks)) + 'API queries')
 
     return results
 
--- core_func/sbb_api_asyncio_wrapper.py
+++ core_func/sbb_api_asyncio_wrapper.py
@@ -1,59 +1,30 @@
-import multiprocessing as mp
-import time
 import os
 import sys
 import io_func
 import core_func
-from core_func.sbb_api_alternative import sbb_query_and_update
-from core_func.sbb_api_2_class import sbb_api_manager
-from core_func.sbb_api_asyncio import async_query_and_process
-from pprint import pprint
-import requests
 import pandas as pd
-from requests.adapters import HTTPAdapter
-from requests.packages.urllib3.util.retry import Retry
-import aiohttp
 import asyncio
 
 
 def primary_wrapper(origin_details, mgdb):
-
-    if mp.cpu_count() < 2:
-        print('Function not set up for less than 2 threads! Terminating.')
-        return 0
-
     try:
-        # Load input file names
-        dir_prefix = ''
-        if __name__ == '__main__':
-            dir_prefix = '../'
-
-        # Initialize various
-        endnodes = (mgdb.get_endnode_set('endnodes_Zurich'))
-        data_set_master = set(endnodes)
+        # Initialize variables
+        data_set_master = set(mgdb.get_endnode_set('endnodes_Zurich'))
+        dest_per_query = 200
 
         # Remove destinations where we already have data
         known_destinations = mgdb.get_destination_set(origin_details)
-        pop_counter = 0
-        for destination in set(endnodes):
+        for destination in set(data_set_master):
             if destination in known_destinations:
                 data_set_master.discard(destination)
-                pop_counter += 1
-        print('discarded ', pop_counter, " destinations, out of ", (len(data_set_master) + pop_counter))
-        # if pop_counter >= 2750:
-            # return 1
-
-        # t_init = time.time()
-        dest_per_query = 200
 
         results = asyncio.run(core_func.sbb_api_asyncio.async_api_handler(origin_details, data_set_master, dest_per_query))
         print(results)
-    # except KeyboardInterrupt or EOFError:
-    #     mgdb.write_data_dict_of_dict(results)
+
     except Exception as e:
         exc_type, exc_obj, exc_tb = sys.exc_info()
         fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-        # print(exc_type, fname, exc_tb.tb_lineno, e)
+        print(exc_type, fname, exc_tb.tb_lineno, e)
         raise
 
     return 1
