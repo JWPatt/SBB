f7e8db0df36854a0897e67cd33e41cdff2c1517a
JWPatt <jamespatterson1989@gmail.com>
2021-10-03 15:52:10 +0200

    - Implementing docstrings in sbb_api_asyncio.py - Cleaning up osrm.py

--- core_func/osrm.py
+++ core_func/osrm.py
@@ -1,84 +1,43 @@
-import multiprocessing as mp
 import time
-import os
-import sys
-import io_func
-import core_func
-from core_func.sbb_api_alternative import sbb_query_and_update
-from pprint import pprint
 import requests
-import pandas as pd
-from requests.adapters import HTTPAdapter
-from requests.packages.urllib3.util.retry import Retry
-from difflib import get_close_matches
-# from io_func.mongodb_crud import write_osrm_line_to_mongodb
 
-def osrm_query(origin_city, dest_cities, city_latlon_table, osrm_url, req_session):
-    t_init = time.time()
-    # for dest_city in dest_cities:
+
+def osrm_query(origin_city, city_latlon_dict, osrm_url, req_session):
+    """This script queries and processes the response from the Open Streetmap Routing Machine.
+
+    Args:
+        origin_city (str): Origin city
+        city_latlon_dict (dict of dict): Dictionary of lat/lon coordinates, with destination cities as keys
+        osrm_url (str): URL of the OSRM service
+        req_session (requests.Session): HTTP session
+
+    Returns:
+        dict of dict: Dictionary of destinations, lat/lon coordinates, and drive times
+    """
+    try:
+        origin_latlon = city_latlon_dict[origin_city]
+    except KeyError:
+        raise KeyError("The Origin city is not a key within the city data dict. Check the spelling (special characters"
+                       "especially) and capitalization.")
+
     count = 0
-    print (city_latlon_table)
-    print (type(city_latlon_table))
-    return_dict = dict(city_latlon_table)
-    for dest_city in dest_cities:
+    return_dict = dict(city_latlon_dict)
+    for dest_city, dest_values in city_latlon_dict.items():
         while True:
             try:
                 count += 1
-                origin_latlon = city_latlon_table[origin_city]
-                dest_latlon = city_latlon_table[dest_city]
-                query_url = osrm_url + '/route/v1/driving/' + str(origin_latlon['lon']) + ',' + str(origin_latlon['lat']) +';'
-                query_url = query_url + str(dest_latlon['lon']) + ',' + str(dest_latlon['lat']) + '?steps=false'
-                print(query_url)
+                query_url = f"{osrm_url}/route/v1/driving/{origin_latlon['lon']},{origin_latlon['lat']};" \
+                            f"{dest_values['lon']},{dest_values['lat']}?steps=false"
 
                 response = req_session.get(query_url)
                 jdata = response.json()
                 return_dict[dest_city] = {}
                 if response.status_code == 200:
-                    print(count)
-                    return_dict[dest_city]['drive_time'] = jdata['routes'][0]['duration']
-                    return_dict[dest_city]['destination'] = dest_city
-                    return_dict[dest_city]['lat'] = city_latlon_table[dest_city]['lat']
-                    return_dict[dest_city]['lon'] = city_latlon_table[dest_city]['lon']
-                # if count > 10: break
+                    city_latlon_dict[dest_city]['drive_time'] = jdata['routes'][0]['duration']
                 break
             except ConnectionError:
                 continue
-    print(time.time() - t_init)
 
     return return_dict
 
-
-
-if __name__ == "__main__":
-    # list_of_locations = {
-    #     "Zurich HB": {"lat": 47.3779, "lon": 8.5403},
-    #     "Bern": {"lat": 46.9490, "lon": 7.4385},
-    #     "Geneva": {"lat": 46.2044, "lon": 6.1413},
-    #     "Lugano": {"lat": 46.0037, "lon": 8.9511},
-    #     "Basel": {"lat": 47.5596, "lon": 7.5886},
-    #     "Lausanne": {"lat": 46.5197, "lon": 6.6323},
-    #     "Sion": {"lat": 46.2331, "lon": 7.3606},
-    # }
-
-    session = requests.Session()
-    url = pd.read_csv("../io_func/secret_mgdb_pw.csv")
-    mgdb = io_func.MongodbHandler(url, "SBB_time_map")
-    locations = mgdb.get_endnode_dict('Zurich_HB.2021_06_26.7_00')
-    list_of_locations = list(locations.keys())
-    print(list_of_locations)
-
-    # start = 'Lugano'
-    # start = get_close_matches(start,list_of_locations)[0]
-    # print(start)
-
-    # list_of_locations = ['Lugano','Bern']
-    # print(locations)
-    locations = osrm_query('Lugano, Autosilo Balestra', list_of_locations, locations, 'http://127.0.0.1:5000', session)
-    # print(locations)
-    df = pd.DataFrame.from_dict(locations, orient='index')
-    # df = pd.DataFrame(list_of_locations)
-    # df.to_csv('osrm.csv')
-    # print(df)
-    # for key in locations:
-        # write_osrm_line_to_mongodb(url, "SBB_time_map", "Zurich", locations[key])
 
--- core_func/sbb_api_asyncio.py
+++ core_func/sbb_api_asyncio.py
@@ -5,30 +5,45 @@ import aiohttp
 import asyncio
 
 
-# Queries and processes the timetable.search.ch API
-async def async_query_and_process(origin_details, destination_list, session):
+async def async_query_and_process(_origin_details, destination_list, session):
+    """Queries and processes results from the timetable.search.ch API
+
+    Args:
+        _origin_details: A list containing: [0] origin location, [1] date, and [2] starting time
+        destination_list: A list of destinations to be submitted in a single API query
+        session: The HTTP session (can be an Asyncio HTTP session)
+
+    Returns:
+        dict: A dictionary of the processed results from a single API request
+    """
+
+    # Must supply a time and date. Saturday at 7:00 is a realistic time for hikers to begin.
     travel_start_date = '2021-06-25'
     travel_start_time = '7:00'
 
-    url = create_url([origin_details, travel_start_date, travel_start_time], destination_list)
+    url = create_url([_origin_details, travel_start_date, travel_start_time], destination_list)
 
-    t_init = time.time()
     async with session.get(url) as response:
-        # TODO: replace this print and the time stamping with proper logging
-        print(response)
         output_data_portion = await asyncio_process_response(response)
-        td_get = time.time() - t_init
 
     return output_data_portion
 
 
-# Takes in the origin details and list of destinations and creates an appropriate url to get
-def create_url (origin_details, destination_list):
+def create_url(_origin_details, destination_list):
+    """Takes in the origin details and list of destinations and creates the timetable.search.ch url to query
+
+    Args:
+        _origin_details: A list containing: [0] origin location, [1] date, and [2] starting time
+        destination_list: A list of destinations to be submitted in a single API query
+
+    Returns:
+        str: A URL to be used to query the timetable.search.ch API; supports multiple destinations per query
+    """
     prefix = 'https://timetable.search.ch/api/route.json?one_to_many=1'
 
-    origin_body = f'&from={origin_details[0]}&date={origin_details[1]}&time={origin_details[2]}'
+    origin_body = f'&from={_origin_details[0]}&date={_origin_details[1]}&time={_origin_details[2]}'
 
-    # Build iterative with necessary syntax between destinations
+    # Build iteratively with necessary syntax between destinations
     destination_body = ''
     for i, dest in enumerate(destination_list):
         destination_body = f'{destination_body}&to[{i}]={dest}'
@@ -36,14 +51,31 @@ def create_url (origin_details, destination_list):
     return f'{prefix}{origin_body}{destination_body}'
 
 
-# Processes the response using asyncio
 async def asyncio_process_response(response):
+    """Wraps the process_response function for use with asyncio (must use "await" keyword). Appears redundant,
+    but is necessary for asyncio to work correctly.
+
+    Args:
+        response: The raw API response
+
+    Returns:
+        json: A dictionary of the processed results from a single API request (via process_response)
+    """
+    # Simply awaiting the response throws a TypeError
     jdata = await response.json()
     return process_response(response, jdata)
 
 
-# Processes response and convert the json into a list of destinations with coordinates and travel times
 def process_response(response, jdata):
+    """Processes response and json reponse into a list of destinations with coordinates and travel times.
+
+    Args:
+        response: The raw API response
+        jdata: JSONified API response
+
+    Returns:
+        A dictionary of the processed results from a single API request
+    """
     data_portions = []
     if response.status != 200:
         print("ERROR: " + str(response.status) + ": " + str(jdata['errors'][0]['message']))
@@ -55,7 +87,7 @@ def process_response(response, jdata):
             print("ERROR: API returned with no results.")
             return {}
 
-    data_portions.append({jdata['results'][0]['points'][0]['text'] :
+    data_portions.append({jdata['results'][0]['points'][0]['text']:
                          {'destination': jdata['results'][0]['points'][0]['text'],
                           'lon': jdata['results'][0]['points'][0]['lon'],
                           'lat': jdata['results'][0]['points'][0]['lat'],
@@ -167,31 +199,50 @@ def process_response(response, jdata):
 
 
 def datetime_to_timestamp(datetime_str):
+    """
+    Converts a date&time string to datetime format
+    Args:
+        datetime_str (str): String of date & time in this format: "YYYY-MM-DD HH:MM:SS"
+
+    Returns:
+        datetime: Datetime value of the input string
+    """
     return datetime.datetime.strptime(datetime_str, "%Y-%m-%d %H:%M:%S").timestamp()
 
 
-async def async_api_handler(origin_details, data_set_master, dest_per_query):
+async def async_api_handler(_origin_details, data_set_master, dest_per_query):
+    """Queries SBB API, processes response, and returns dictionary of locations with travel information.
+
+    Args:
+        _origin_details: A list containing: [0] origin location, [1] date, and [2] starting time
+        data_set_master: A list of all destinations, to be submitted piece-wise in multiple API queries
+        dest_per_query: The number of destinations to submit per API query (timetable.search.ch has max of ~220)
+
+    Returns:
+        dict (str: dict): The processed and compiled results of all API queries. Keys are destination names; nested
+            dicts contain relevant data: travel time, lat/lon, # of stops, etc.
+    """
     t_init = time.time()
-    n_queries = -(-len(data_set_master)//dest_per_query)  #ceiling integer division
     async with aiohttp.ClientSession() as session:
         print('Opening AsyncIO HTTP session.')
+
+        # Break up list of destinations into chunks of a particular size (the SBB API has a ceiling around 210)
         destination_chunks = [list(data_set_master)[i:i + dest_per_query] for i in range(0, len(data_set_master), dest_per_query)]
-        get_reqs = []
+        get_requests = []
         for dest_list in destination_chunks:
-            get_reqs.append(asyncio.ensure_future(async_query_and_process(origin_details, dest_list, session)))
-        results_list = await asyncio.gather(*get_reqs)
+            get_requests.append(asyncio.ensure_future(async_query_and_process(_origin_details, dest_list, session)))
+        print(f'Awaiting response from {len(get_requests)} requests.')
+        results_list = await asyncio.gather(*get_requests)
 
     results = {}
     for result in results_list:
         results.update(result)
 
-    print('Time to clear the stack: ' + str(time.time() - t_init) + ' seconds, and ' + str(len(destination_chunks)) + 'API queries')
-
+    print(f'{len(destination_chunks)} API queries took {time.time() - t_init} seconds to receive and process')
     return results
 
 
 if __name__ == "__main__":
     data_list = ['Bern', 'Thun', 'Interlaken Ost']
     origin_details = ['Zurich HB', '2021-06-25', '7:00']
-    asyncio.run(async_api_handler(origin_details, data_list, 2 ))
-    print('done')
+    asyncio.run(async_api_handler(origin_details, data_list, 2))
