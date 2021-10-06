6c1ec7f976d5455566b455687bb3853a85c33af3
JWPatt <jamespatterson1989@gmail.com>
2021-10-06 23:23:31 +0200

    removed old files/functions

--- io_func/__init__.py
+++ io_func/__init__.py
@@ -1,12 +1,3 @@
-from .read_in_csv import betriebspunkt_csv_to_empty_dict
-from .read_in_csv import csv_to_dict
-from .read_in_csv import csv_to_empty_dict
-from .read_in_csv import csv_to_set
-from .write_to_csv import write_destination_set_to_csv
-from .write_to_csv import write_data_line_to_open_csv
-from .write_to_csv import write_data_to_open_csv
-from .write_to_csv import write_destination_to_csv
-from .database_name_maker import reformat_origin_details
 from .mongodb_crud import MongodbHandler
 from .status_bar import status_bar_printer
 
