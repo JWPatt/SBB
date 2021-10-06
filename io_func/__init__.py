5f760c990ba7e980314baaff6066c69af217cc33
JWPatt <jamespatterson1989@gmail.com>
2021-10-06 23:12:49 +0200

    removed "import *"

--- io_func/__init__.py
+++ io_func/__init__.py
@@ -1,5 +1,3 @@
-# they say using the "import *" syntax is bad practice, but idk in this case it would be nice...
-
 from .read_in_csv import betriebspunkt_csv_to_empty_dict
 from .read_in_csv import csv_to_dict
 from .read_in_csv import csv_to_empty_dict
@@ -10,5 +8,5 @@ from .write_to_csv import write_data_to_open_csv
 from .write_to_csv import write_destination_to_csv
 from .database_name_maker import reformat_origin_details
 from .mongodb_crud import MongodbHandler
-from .status_bar import *
+from .status_bar import status_bar_printer
 
