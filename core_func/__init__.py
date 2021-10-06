20459fe09e2221b1dcc7782b33bc97f6d49730b2
JWPatt <jamespatterson1989@gmail.com>
2021-10-06 23:22:54 +0200

    removed old files/functions

--- core_func/__init__.py
+++ core_func/__init__.py
@@ -1,12 +1,4 @@
-# they say using the "import *" syntax is bad practice, but idk in this case it would be nice...
-
-from .process_data import process_data
-from .process_data import listen_and_write
-from .process_data import listen_and_spawn_job
-from .process_data import update_dict_min_duration
-from .sbb_api_alternative import sbb_query_and_update
 from .sbb_api_asyncio import *
-from .html_plot import make_html_map
 from .sbb_api_asyncio_wrapper import sbb_api_asyncio_wrapper
 from .misc_funcs import *
 from .osrm import *
