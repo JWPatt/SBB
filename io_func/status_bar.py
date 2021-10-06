63fafb2e85970e3c22bddd9e6f9af999fa55bf18
JWPatt <jamespatterson1989@gmail.com>
2021-10-06 23:10:50 +0200

    updated docstring

--- io_func/status_bar.py
+++ io_func/status_bar.py
@@ -7,6 +7,7 @@ def status_bar_printer(count, total, suffix):
     Args:
         count (int): The number of queries performed already
         total (int): The number of total destinations to be queried
+        suffix (str): A bit of text at the end of the output
 
     Returns:
         None - prints status bar, percentage completion, and # of queries performed
@@ -15,7 +16,3 @@ def status_bar_printer(count, total, suffix):
     progress = count / total * 20
     sys.stdout.write("[%-20s] %d%% - %i of %i %s" % ('=' * int(progress), progress * 5, count, total, suffix))
     sys.stdout.flush()
-    #
-    # progress = count / total * 20
-    # print("[%-20s] %d%% - %i of %i" % ('=' * int(progress), progress * 5, count, total), end='\r')
-
